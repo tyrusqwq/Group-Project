2d71b10a29fe7f44e1152e3ae8b47d196432761f
Ho Lam HO <24053718A@common.cpce-polyu.edu.hk>
2025-03-30 00:13:48 +0800

    Add project files.

---
+++ Group Project/Source.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+
+	//SMS menu
+	int num_selected;//which numbers that user input
+	cout << endl;
+	std::cout << "*** SMS Main Menu ***" << endl;//text
+	std::cout << "[1] Load Starting Data";
+	std::cout << "\n[2] Show Student Records";
+	std::cout << "\n[3] Add / Delete Students";
+	std::cout << "\n[4] Edit Students";
+	std::cout << "\n[5] Generate Transcript";
+	std::cout << "\n[6] Credits and Exit" << endl;
+	std::cout << "*************************\n";
+	cout << "options 1 ~ 6" << endl;
+	std::cin >> num_selected;//which numbers that user input
+
+
+
+	switch (num_selected)//switch used for detect what the user input
+	{
+	case 1:
+	{
+		//Load Starting Data
+		break;
+	}
+	case 2:
+	{
+		//Show Student Records
+		break;
+	}
+	case 3:
+	{
+		//Add / Delete Students
+		break;
+	}
+	case 4:
+	{
+		//Edit Students
+		break;
+	}
+	case 5:
+	{
+		//Generate Transcript
+		break;
+	}
+	case 6:
+	{
+		//Credits and Exit
+		break;
+	}
+	default:
+	{
+		cout << "The number is not in 1-6";
+	}
+	}
+	return 0;
+}
